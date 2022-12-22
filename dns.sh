0efb2beaf82faefe9215167ab55f76ad7dc70cfe
templarfelix <contato@templarfelix.com>
2022-12-21 21:23:41 -0300

    d

--- dns.sh
+++ dns.sh
@@ -3,24 +3,26 @@
 sudo dnf -y install bind bind-utils
 sudo systemctl enable named --now
 
-sudo systemctl status named
 sudo firewall-cmd --add-service=dns --permanent
+sudo firewall-cmd --add-service=dhcp --permanent
 sudo firewall-cmd --reload
 
 sudo sed -e "s:allow-query.*:allow-query { 192.169.0.0/16;  172.16.0.0/16; localhost; };:g" \
     -e "s:listen-on port .*:listen-on port 53 { 172.16.226.10; };:g" \
     -i /etc/named.conf
 sudo systemctl restart named
+sudo systemctl restart dnsmasq
 
 # FIXME TEM Q CORRIGIR dnsmasq 
 
 ## REMOVER
-#sudo rm -rf /etc/resolv.conf
-#cat <<EOF | sudo tee -a /etc/resolv.conf
-#nameserver 127.0.0.53
-#options edns0 trust-ad
-#search localdomain
-#EOF
+sudo rm -rf /etc/resolv.conf
+cat <<EOF | sudo tee -a /etc/resolv.conf
+nameserver 127.0.0.53
+nameserver 172.16.226.10
+options edns0 trust-ad
+search localdomain
+EOF
     
 cat <<EOF | sudo tee -a /etc/named.conf
 include "/etc/named.k8s.zones";
@@ -32,9 +34,9 @@ key "externaldns-key" {
   algorithm hmac-sha256;
   secret "96Ah/a2g0/nLeFGK+d/0tzQcccf9hCEIy34PoXX2Qg8=";
 };
-zone "k8s.templarfelix.local" {
+zone "templarfelix.local" {
     type master;
-    file "k8s.templarfelix.local.zone";
+    file "templarfelix.local.zone";
     allow-transfer {
         key "externaldns-key";
     };
@@ -44,18 +46,52 @@ zone "k8s.templarfelix.local" {
 };
 EOF
 
-sudo rm -rf /var/named/k8s.templarfelix.local.zone
-cat <<EOF | sudo tee -a /var/named/k8s.templarfelix.local.zone
-\$TTL 86400
-
-@ IN SOA k8s.templarfelix.local root.k8s.templarfelix.local (
-  2017010302
-  3600
-  900
-  604800
-  86400
+sudo rm -rf /var/named/templarfelix.local.zone
+cat <<EOF | sudo tee -a /var/named/templarfelix.local.zone
+\$TTL 60
+@ IN SOA templarfelix.local root.templarfelix.local (
+  16
+  60
+  60
+  60
+  0
 )
 
 @      IN NS server
 server IN A  192.168.184.131
 EOF
+
+sudo rm -rf /var/named/templarfelix.local.zone
+cat <<EOF | sudo tee -a /var/named/templarfelix.local.zone
+$TTL 60 ; 1 minute
+@         IN SOA  templarfelix.local. root.templarfelix.local. (
+                                16         ; serial
+                                60         ; refresh (1 minute)
+                                60         ; retry (1 minute)
+                                60         ; expire (1 minute)
+                                60         ; minimum (1 minute)
+                                )
+                        NS      ns.templarfelix.local.
+ns                    A       1.1.1.1
+EOF
+
+sudo systemctl restart named
+sudo systemctl restart dnsmasq
+
+# DEBUG
+journalctl -xeu named.service
+
+sudo systemctl status named
+sudo systemctl status dnsmasq
+
+
+
+nslookup server.templarfelix.local
+nslookup 172.16.226.10
+
+dig -x templarfelix.local
+sudo nslookup kafka-bootstrap.templarfelix.local.
+
+dig templarfelix.local ANY +noall +answer
+
+sudo nslookup kafka-broker-0.templarfelix.local.
